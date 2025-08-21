1b30dc9ddeb0169ba1d31279d8f854483922d105
ahmad-mridul <mridulahmed262@gmail.com>
2025-08-21 16:07:06 +0600

    login logout implemented

--- members/urls.py
+++ members/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('test',views.testing,name='testing'),
     path('register',views.register,name='register'),
     path('login',views.login,name='login'),
+    path('logout',views.logout,name='logout'),
 ]
--- members/views.py
+++ members/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User, auth
-from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
 from .models import Member
 from django.db.models import Q
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
+
+
+
 
 # Create your views here.
 def members(request):
@@ -37,11 +40,11 @@ def register(request):
         password = request.POST['password']
         password2 = request.POST['password2']
         if password == password2:
-            if User.objects.filter(email=email).exists():
-                messages.info(request,'email already used ')
-                return redirect('register')
-            elif User.objects.filter(username=username).exists():
+            if User.objects.filter(username=username).exists():
                 messages.info(request,'user name already used')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'email already used')
                 return redirect('register')
             else:
                 user = User.objects.create_user(username=username,email=email,password=password)
@@ -54,15 +57,20 @@ def register(request):
         return render(request,'register.html')
 
 def login(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         username = request.POST['name']
         password = request.POST['password']
-        user = authenticate(username=username,password=password)
+        user = auth.authenticate(username=username,password=password)
         if user is not None:
             auth.login(request,user)
             return redirect('/')
         else:
-            messages.info(request,'Credential is invalid')
+            messages.info(request,'credentials invalid')
             return redirect('login')
     else:
         return render(request,'login.html')
+    
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
